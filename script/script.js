3aa988a3c397ca813156336963926b9b1835d7fc
usmanow <adel.usmanoff@yandex.ru>
2024-12-27 19:53:55 +0300

    mvc practice

---
+++ script/script.js
@@ -0,0 +1,153 @@
+const watchedState = new Proxy({
+  productList: [],
+  isModalOpen: false,
+  isLoaderOpen: false
+}, {
+  set: (target, prop, value) => {
+    target[prop] = value
+
+    if (prop === 'productList') {
+      renderProducts(target[prop])
+    }
+
+    if (prop === 'isLoaderOpen') {
+      loaderRender(value)
+    }
+
+    if (prop === 'isModalOpen') {
+      toggleModalVisibility(value)
+    }
+
+    return true
+  }
+})
+
+const fetchProducts = async () => {
+  try {
+    watchedState.isLoaderOpen = true
+    const response = await fetch('https://fakestores.onrender.com/api/products')
+    const products = await response.json()
+    watchedState.productList = products.slice(0, 10)
+    renderProducts(watchedState.productList)
+  } catch (error) {
+    console.error('Ошибка', error)
+  } finally {
+    watchedState.isLoaderOpen = false
+  }
+}
+
+const renderProducts = (products) => {
+  const productContainer = document.querySelector('.products-list')
+  productContainer.innerHTML = ''
+
+  products.forEach((product) => {
+    const liElement = document.createElement('li')
+    liElement.classList.add('products-list__item')
+    liElement.id = product.id
+
+    const title = document.createElement('h1')
+    title.classList.add('products-list__title')
+    title.textContent = product.title
+
+    const img = document.createElement('img')
+    img.classList.add('products-list__image')
+    img.src = product.imageURL
+    img.width = '170'
+    img.height = '170'
+    img.alt = product.title
+
+    const descriptionContainer = document.createElement('div')
+    descriptionContainer.classList.add('products-list__description-wrapper')
+
+    const description = document.createElement('span')
+    description.classList.add('products-list__description')
+    description.textContent = product.description
+
+    const openModalButton = document.createElement('button')
+    openModalButton.classList.add('products-list__detail-button')
+    openModalButton.setAttribute('data-id', product.id)
+    openModalButton.textContent = 'подробнее'
+    openModalButton.setAttribute('data-id', product.id)
+
+    const price = document.createElement('span')
+    price.classList.add('products-list__price')
+    price.textContent = `$ ${product.price}`
+
+    const button = document.createElement('button')
+    button.classList.add('products-list__button')
+    button.classList.add('button')
+    button.textContent = 'Купить'
+
+    descriptionContainer.append(description, openModalButton)
+    liElement.append(title, img, descriptionContainer, price, button)
+    productContainer.append(liElement)
+
+    openModalButton.addEventListener('click', () => toggleModal(product))
+  })
+}
+
+const updateModalContent = (product) => {
+  const title = document.querySelector('.modal__product-title')
+  const image = document.querySelector('.modal__product-image')
+  const description = document.querySelector('.modal__product-description')
+  const price = document.querySelector('.modal__product-price')
+  const button = document.querySelector('.modal__product-button')
+
+  title.textContent = product.title
+  image.src = product.imageURL
+  image.alt = product.title
+  description.textContent = product.description
+  price.textContent = `$ ${product.price}`
+  button.textContent = 'Купить'
+}
+
+const toggleModal = (product) => {
+  updateModalContent(product)
+  watchedState.isModalOpen = true
+}
+
+const toggleModalVisibility = (isVisible) => {
+  const modal = document.querySelector('.modal')
+  if (isVisible) {
+    modal.classList.add('show')
+  } else {
+    modal.classList.remove('show')
+  }
+}
+
+const addModalCloseListeners = () => {
+  const modal = document.querySelector('.modal__overlay')
+  const closeModalButton = document.querySelector('.modal__close-button')
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) watchedState.isModalOpen = false
+  })
+
+  closeModalButton.addEventListener('click', () => {
+    watchedState.isModalOpen = false
+  })
+}
+
+const loaderRender = (isLoading) => {
+  const loader = document.querySelector('.loader')
+  if (!isLoading) {
+    loader.classList.add('none')
+  } else {
+    loader.classList.remove('none')
+  }
+}
+
+const handleSearch = (e) => {
+  const inputValue = e.target.value.toLowerCase()
+  const filteredProducts = watchedState.productList.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue)
+  })
+
+  renderProducts(filteredProducts)
+}
+
+const inputSearch = document.querySelector('.header__search')
+inputSearch.addEventListener('input', handleSearch)
+
+addModalCloseListeners()
+fetchProducts()
